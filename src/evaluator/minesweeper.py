192b91c0672e0a6e4d3ea69b5ef6384fb10fcead
ljcleo <ljcleo@qq.com>
2025-11-16 15:13:17 +0800

    update evaluation script update lock file

--- src/evaluator/minesweeper.py
+++ src/evaluator/minesweeper.py
@@ -2,6 +2,7 @@ from collections.abc import Mapping
 from typing import override
 
 from .base import Evaluator
+from .utils import compress_json
 
 
 class MinesweeperEvaluator(Evaluator):
@@ -13,4 +14,4 @@ class MinesweeperEvaluator(Evaluator):
     @override
     @staticmethod
     def check_solution(example: Mapping[str, str]) -> bool:
-        return example["proposal"] == example["label"]
+        return compress_json(example["proposal"]) == compress_json(example["label"])
--- src/evaluator/utils.py
+++ src/evaluator/utils.py
@@ -1,5 +1,10 @@
+import json
 from pydantic import TypeAdapter
 
 
 def load_json[T](obj_type: type[T], doc: str) -> T:
     return TypeAdapter(obj_type).validate_json(doc)
+
+
+def compress_json(doc: str) -> str:
+    return json.dumps(json.loads(doc), ensure_ascii=False, separators=(",", ":"))
